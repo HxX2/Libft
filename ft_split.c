7933bdc6412f516fe3de1201495ecadc3e4a7a6f
HxX2 <zaka.hanix@gmail.com>
2021-11-20 05:25:45 +0100

    split need free

--- ft_split.c
+++ ft_split.c
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include "libft.h"
 
 int	ft_wcntr(char const *s, char c)
 {
@@ -31,12 +32,43 @@ int	ft_wcntr(char const *s, char c)
 	return (nw);
 }
 
-// char **ft_split(char const *s, char c);
-
-int main()
+char	**ft_split(char const *s, char c)
 {
-	char const str[] = "lkl  kkh  hklfh  fhklk    lkhfj";
-	char c = ' ';
+	int		i;
+	int		j;
+	char	**p;
+	int		wstart;
 
-	printf("%d", ft_wcntr(str, c));
+	i = 0;
+	j = -1;
+	p = malloc((ft_wcntr(s, c) + 1) * sizeof(char *));
+	if (!p)
+		return (NULL);
+	while (s[i])
+	{
+		while (s[i] == c)
+			wstart = ++i;
+		while (s[i] != c && s[i])
+			i++;
+		if (j <= ft_wcntr(s, c) && (i - wstart) > 0)
+			p[++j] = ft_substr(s, wstart, (i - wstart));
+		// if (!p[j])
+			// return (ft_wipe);
+	}
+	p[j + 1] = NULL;
+	// ft_wipe(p);
+	return (p);
 }
+
+// int main()
+// {
+// 	char const str[] = "hlk   k hh hh h";
+// 	char c = ' ';
+// 	char **p;
+
+// 	p = ft_split(str, c);
+// 	for (size_t i = 0; i < ft_wcntr(str, c); i++)
+// 	{
+// 		printf("%s\n", p[i]);
+// 	}	
+// }
